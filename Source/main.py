2cb2993ba8a49a844697f0150940e695ec1986ca
spiderhacker <ayan.rockstar1@gmail.com>
2015-03-11 19:21:07 +0530

    Project Initialized
    Python source added

---
+++ Source/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# An obstacle avoiding ball simulation using ML
+
+"""
+Machine Learing
+Artificial Intelligence
+"""
+
+def main():
+    print 'Hello AI & ML'
+
+if(__name__=='__main__'):
+    main()
